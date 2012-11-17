92f29313dce53ae749dbb37067779e1fb0cf2c95
Cyrus Anthony Custodio <cyrus916@gmail.com>
2012-11-17 16:15:09 -0600

    added admin dashboard
    used activeadmin gem

--- pampicture/config/routes.rb
+++ pampicture/config/routes.rb
@@ -1,7 +1,13 @@
 Pampicture::Application.routes.draw do
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
   resources :categories
 
   resources :products
+  
+  root :to => "products#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
