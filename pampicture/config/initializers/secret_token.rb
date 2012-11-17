7561f6a19295e948b8de97a51deb7873b2e1243c
Cyrus Anthony Custodio <cyrus916@gmail.com>
2012-11-17 15:21:49 -0600

    init

---
+++ pampicture/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pampicture::Application.config.secret_token = '88aceed0ba618b28c8a1b8483c8f2125eab7934058843b8ad12fde4c56b3eaa449fb5c23674d65f34ea165076111973b6844d4bf4bc4f6c84205fe35bcaa65ba'
