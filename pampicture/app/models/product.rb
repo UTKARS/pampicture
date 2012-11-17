6b8111108e3eb6c3b278b4d6b4abb90ff9459fc6
Cyrus Anthony Custodio <cyrus916@gmail.com>
2012-11-17 16:20:26 -0600

    modified product + category models
    validations, relationship

--- pampicture/app/models/product.rb
+++ pampicture/app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ActiveRecord::Base
+  attr_accessible :name, :description, :price, :stock_quantity, :category_id, :image
+  validates_presence_of :name, :description, :price, :stock_quantity, :category_id
+  validates :name, :uniqueness => true
+  belongs_to :category
 end
